fa3d46c942c39b73a64d37eba6c2a4c544eed0ed
ANyholt-lssd <98838392+ANyholt-lssd@users.noreply.github.com>
2022-03-18 09:51:40 -0600

    troubleshooting

--- heartbeat.py
+++ heartbeat.py
@@ -21,6 +21,7 @@ def serialize(things):
         string += things[key]
         string += '"'
     string += "}"
+    return string
 def get_serial():
     with open("/proc/cpuinfo","r") as f:
         for line in f:
