fa4700f01507bae027d4794196686b007e2a76ce
Mark Strendin <mark.strendin@lskysd.ca>
2022-03-29 16:23:51 -0600

    Fix broken update mechanism - I hope... Since the working directory in a cron job is the user's home directory, running git pull without specifying a directory will fail, so the pis will never actually update. We may need to implement a better update mechanism, but I think this should work for now.

--- heartbeat.py
+++ heartbeat.py
@@ -150,7 +150,6 @@ if not api_success:
     display_error_text_on_screen("COMMS FAIL".format(attempt_number), 20)
     quit()
 
-
 # #########################################################
 # Check the software version based on API response
 # #########################################################
@@ -166,8 +165,9 @@ log_this(f"Found remote version: {api_response['versionNumber']}")
 
 if api_response["versionNumber"] != local_version:
     display_text_on_screen("Updating...", 20)
-    log_this("Performing git pull")
-    system("git pull --no-rebase")
+    log_this(f"Performing git pull in {current_directory}")
+    system(f"cd {current_directory}")
+    system("git pull")
     log_this("Updating version number")
     with open(current_directory +"version","w") as version_file:
         display_text_on_screen(api_response['versionNumber'], 20)
