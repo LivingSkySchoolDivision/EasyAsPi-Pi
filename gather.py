d5b7f66a8ece22d601b90d9cf2baeb50706b463f
Mark Strendin <mark.strendin@lskysd.ca>
2022-04-04 14:10:56 -0600

    Some cleanup on gather script (#3)
    * Store version number in the repo, rather than getting it from the API.
    * Show version number on onboard screen
    * Clean up gather script a bit

--- gather.py
+++ gather.py
@@ -1,137 +1,118 @@
 import logging
 logging.basicConfig(filename = "/var/log/EasyAsPi",level = logging.DEBUG, format = "%(asctime)s:%(levelname)s:on line %(lineno)d:%(message)s")
-logging.debug("Imported logging")
-logging.debug("Importing gpiozero")
 import gpiozero
-logging.debug("gpiozero Imported")
-logging.debug("importing requests")
 import requests
-logging.debug("requests imported")
-logging.debug("importing bme280")
 from bme280 import BME280
-logging.debug("bme280 imported")
-logging.debug("importing ltr559")
 import ltr559
-logging.debug("ltr559 imported")
-logging.debug("importing gas")
 from enviroplus import gas
-logging.debug("gas imported")
-logging.debug("creating the temperature adjustment")
+from os import path, system, getcwd
+
 temp_compensate = 1.45
-logging.debug("temperature adjustment created")
-logging.debug("assigning the URL")
-URL = "https://envirosaurus.lskysd.ca/SensorReading"
-logging.debug("URL assigned")
 
-def serialize(things):
-    logging.debug("Begin serializing")
-    string = "{"
+API_URL = "https://envirosaurus.lskysd.ca/SensorReading"
+
+# #########################################################
+# Function definitions and initialization stuff
+# #########################################################
+
+def log_this(text):
+    print(text)
+    logging.info(text)
+
+def convert_to_json(obj):
+    returned_json = "{"
     is_first = True
-    for key in things:
+    for key in obj:
         if  not is_first:
-            string += ","
+            returned_json += ","
         is_first = False
-        string += '"'
-        string += key
-        string += '"'
-        string += ":"
-        if type(things[key]) == str:
-            string += '"'
-            string += things[key]
-            string += '"'
-        elif type(things[key]) == bool:
-            if things[key]:
-                string += "true"
+        returned_json += '"'
+        returned_json += key
+        returned_json += '"'
+        returned_json += ":"
+        if type(obj[key]) == str:
+            returned_json += '"'
+            returned_json += obj[key]
+            returned_json += '"'
+        elif type(obj[key]) == bool:
+            if obj[key]:
+                returned_json += "true"
             else:
-                string += "false"
+                returned_json += "false"
         else:
-            string += str(things[key])
-        
-    string += "}"
-    logging.debug("end serializing")
-    return string
-def get_serial():
-    logging.info("fetching serial number")
-    logging.debug("opening cpuinfo file as read")
-    with open("/proc/cpuinfo","r") as f:
-        logging.debug("cpuinfo file opened as read")
-        logging.debug("reading lines")
-        for line in f:
-            logging.debug("Checking if line starts with 'Serial'")
-            if line[0:6]=="Serial":
-                logging.debug("Line began with 'Serial'")
-                logging.debug("returning characters 10 to -1")
-                return line[10:-1]
-logging.debug("creating empty dictionary")
-my_dict = {}
-logging.debug("created empty dictionary")
-logging.debug("creating object for temperature, pressure and humidity sensor")
-tph = BME280()
-logging.debug("object for temperature, pressure and humidity sensor created")
-logging.debug("Creating light sensor object")
-light = ltr559.LTR559()
-logging.debug("crerated object for light sensor")
+            returned_json += str(obj[key])
+    returned_json += "}"
+    return returned_json
 
-logging.info("reading temperature, pressure and humidity")
-logging.debug("updating tph sensor")
+def get_serial():
+    with open("/proc/cpuinfo","r") as f:
+        for line in f:
+            if line[0:6]=="Serial":
+                return line[10:-1].strip()
+
+def get_version():
+    with open(f"{path.dirname(__file__)}/version","r") as version_file:
+        return version_file.readline().strip()
+
+sensor_payload = {}
+
+tph = BME280()
+light = ltr559.LTR559()
+
+# #########################################################
+# Read temperature, pressure, and humidity data
+# #########################################################
+
+log_this("reading temperature, pressure and humidity")
 tph.update_sensor()
-logging.debug("tph sensor updated")
-logging.debug("priming temperature")
 temp_primer = tph.temperature
-logging.debug("primed temperature")
-logging.debug("primning pressure")
 pres_primer = tph.pressure
-logging.debug("pressure primed")
-logging.debug("priming humidity")
 hum_primer = tph.humidity
-logging.debug("humidity primed")
-logging.debug("reading humidity into dictionary")
-my_dict["humidityPercent"] = tph.humidity
-logging.debug("humidity read into dictionary")
-logging.debug("reading pressure into dictionary")
-my_dict["pressure"] = tph.pressure
-logging.debug("pressure read into dictionary")
-logging.debug("reading temperature into dictionary")
-my_dict["temperatureCelsius"] = tph.temperature
-logging.debug("temperature read into dictionary")
-logging.debug("looping until no values match the primers")
-while temp_primer==my_dict["temperatureCelsius"]or pres_primer==my_dict["pressure"]or hum_primer==my_dict["humidityPercent"]:
-    logging.debug("start of loop")
-    logging.debug("updating tph sensor")
+sensor_payload["humidityPercent"] = tph.humidity
+sensor_payload["pressure"] = tph.pressure
+sensor_payload["temperatureCelsius"] = tph.temperature
+while temp_primer==sensor_payload["temperatureCelsius"]or pres_primer==sensor_payload["pressure"]or hum_primer==sensor_payload["humidityPercent"]:
     tph.update_sensor()
-    logging.debug("tph sensor updated")
-    logging.debug("reading humidity")
-    my_dict["humidityPercent"] = tph.humidity
-    logging.debug("humidity read")
-    logging.debug("reading pressure")
-    my_dict["pressure"] = tph.pressure
-    logging.debug("pressure read")
-    logging.debug("reading temperature")
-    my_dict["temperatureCelsius"] = tph.temperature
-    logging.debug("tempreature read")
-logging.info("reading cpu temperature")
-my_dict["cpuTemperatureCelsius"] = gpiozero.CPUTemperature().temperature
-logging.debug("cputemperature read")
-logging.debug("compensating abient temperature for cpu temperature")
-my_dict["temperatureCelsius"]=my_dict["temperatureCelsius"] - ((my_dict["cpuTemperatureCelsius"] - my_dict["temperatureCelsius"]) / temp_compensate)
-logging.debug("abient temperature compensated")
-logging.info("reading serial number into dictionary")
-my_dict["deviceSerialNumber"] = get_serial().strip()
-logging.debug("serial number read")
-logging.info("reading light levels")
-my_dict["lux"]=light.get_lux()
-logging.debug("light levels read")
-logging.info("reading gasses")
-logging.debug("updating the gasses sensor")
+    sensor_payload["humidityPercent"] = tph.humidity
+    sensor_payload["pressure"] = tph.pressure
+    sensor_payload["temperatureCelsius"] = tph.temperature
+
+
+# #########################################################
+# Read CPU temp
+# #########################################################
+
+log_this("reading cpu temperature")
+sensor_payload["cpuTemperatureCelsius"] = gpiozero.CPUTemperature().temperature
+sensor_payload["temperatureCelsius"]=sensor_payload["temperatureCelsius"] - ((sensor_payload["cpuTemperatureCelsius"] - sensor_payload["temperatureCelsius"]) / temp_compensate)
+
+
+# #########################################################
+# Retrieve serial number
+# #########################################################
+log_this("reading serial number into dictionary")
+sensor_payload["deviceSerialNumber"] = get_serial()
+
+# #########################################################
+# Read lux/light meter
+# #########################################################
+
+log_this("reading light levels")
+sensor_payload["lux"]=light.get_lux()
+
+# #########################################################
+# Read gas sensors
+# #########################################################
+
+log_this("reading gasses")
 gasses = gas.read_all()
-logging.debug("reading the oxidising levels")
-my_dict["oxidisingGasLevel"]=gasses.oxidising
-logging.debug("oxidising levels read")
-logging.debug("reading reducing levels")
-my_dict["reducingGasLevel"]=gasses.reducing
-logging.debug("reducing levels read")
-logging.debug("reading nh3 levels")
-my_dict["nH3Level"]=gasses.nh3
-logging.debug("nh3 levels read")
-logging.info("sending post request")
-logging.info(requests.post(URL,data=serialize(my_dict),headers={"Content-Type":"application/json"}, timeout = 20).json)
+sensor_payload["oxidisingGasLevel"]=gasses.oxidising
+sensor_payload["reducingGasLevel"]=gasses.reducing
+sensor_payload["nH3Level"]=gasses.nh3
+
+# #########################################################
+# Send this payload to the API
+# #########################################################
+
+log_this("sending post request")
+log_this(requests.post(API_URL,data=convert_to_json(sensor_payload),headers={"Content-Type":"application/json"}, timeout = 20).json)
--- heartbeat.py
+++ heartbeat.py
@@ -49,18 +49,28 @@ def get_model():
             if line[0:5]=="Model":
                 return line[9:-1]
 
+def get_version():
+    with open(f"{path.dirname(__file__)}/version","r") as version_file:
+        return version_file.readline().strip()
+
 # Initialize the display
 onboardScreen = ST7735.ST7735(port=0,cs=1,dc=9,backlight=12,rotation=90,spi_speed_hz=10000000)
 onboardScreen.begin()
 
+def display_assigned_number_on_screen(assigned_number, version_number):
+    newImage = Image.new('RGB', (onboardScreen.width, onboardScreen.height), color="Black")
+    ImageDraw.Draw(newImage).text((0,-15),assigned_number, font=ImageFont.truetype(RobotoMedium, 85), fill=(0,100,0))
+    ImageDraw.Draw(newImage).text((0,70),version_number, font=ImageFont.truetype(RobotoMedium, 10), fill=(64,64,64))
+    onboardScreen.display(newImage)
+
 def display_text_on_screen(text, size=40, yoffset=15):
     newImage = Image.new('RGB', (onboardScreen.width, onboardScreen.height), color="Black")
-    ImageDraw.Draw(newImage).text((0,yoffset),text, font=ImageFont.truetype(RobotoMedium, size), fill="Blue")
+    ImageDraw.Draw(newImage).text((0,yoffset),text, font=ImageFont.truetype(RobotoMedium, size), fill="White")
     onboardScreen.display(newImage)
 
 def display_special_text_on_screen(text, size=40, yoffset=15):
-    newImage = Image.new('RGB', (onboardScreen.width, onboardScreen.height), color="Blue")
-    ImageDraw.Draw(newImage).text((0,yoffset),text, font=ImageFont.truetype(RobotoMedium, size), fill="white")
+    newImage = Image.new('RGB', (onboardScreen.width, onboardScreen.height), color="Green")
+    ImageDraw.Draw(newImage).text((0,yoffset),text, font=ImageFont.truetype(RobotoMedium, size), fill="Blue")
     onboardScreen.display(newImage)
 
 def display_error_text_on_screen(text, size=40, yoffset=15):
@@ -72,16 +82,6 @@ display_text_on_screen("Initializing...", 20)
 
 # Initialize a reboot flag that we'll use later
 reboot = False
-
-# #########################################################
-# Check for important files that might need to be created
-# #########################################################
-current_directory= f"{path.dirname(__file__)}/"
-if not path.exists(current_directory +"version"):
-    log_this("Creating empty version file")
-    with open(current_directory +"version", "w") as version_file:
-        version_file.write("DEFAULT")
-
 
 # #########################################################
 # Check to see if we need to change the hostname
@@ -112,7 +112,9 @@ if hostname != expected_hostname:
 api_success = False
 
 # Build heartbeat payload
-heartbeat_request_content = {"deviceSerialNumber":get_serial(),"deviceModel":get_model()}
+local_version = get_version()
+log_this(f"Found local version to be {local_version}")
+heartbeat_request_content = {"deviceSerialNumber":get_serial(),"deviceModel":get_model(),"DeviceVersion":local_version}
 
 # Wait a few seconds for WiFi to connect...
 log_this("Waiting for WiFi...")
@@ -157,26 +159,24 @@ if not api_success:
 log_this("Checking version")
 display_text_on_screen("Version check...", 20)
 
-with open(current_directory +"version","r") as version_file:
-    local_version = version_file.readline().strip()
-
 log_this(f"Found local version: {local_version}")
 log_this(f"Found remote version: {api_response['versionNumber']}")
 
 if api_response["versionNumber"] != local_version:
     display_text_on_screen("Updating...", 20)
-    log_this(f"Performing git pull in {current_directory}")
-    system(f"cd {current_directory} && git fetch --all && git reset --hard origin/main")    
-    log_this("Updating version number")
-    with open(current_directory +"version","w") as version_file:
-        display_text_on_screen(api_response['versionNumber'], 20)
-        version_file.write(api_response["versionNumber"])
+    log_this(f"Performing git pull in {path.dirname(__file__)}")
+    system(f"cd {path.dirname(__file__)} && git fetch --all && git reset --hard origin/main")
+    sleep(10)
     reboot = True
 else:
     display_text_on_screen("Version OK", 20)
+    sleep(1)
     log_this("Version OK")
 
+
+# #########################################################
 # Check if we need to reboot from the reboot flag
+# #########################################################
 if reboot:
     log_this("Rebooting due to flag being raised")
     display_special_text_on_screen("REBOOTING...", 20)
@@ -188,15 +188,15 @@ if reboot:
 # Display the assigned number on the onboard screen
 # #########################################################
 
-text_str = ""
+assigned_number_text = ""
 if api_response["assignedNumber"]<100:
-    text_str+="0"
+    assigned_number_text+="0"
     if api_response["assignedNumber"]<10:
-        text_str+="0"
+        assigned_number_text+="0"
 
-log_this("Displaying assigned number on screen: " + text_str)
-text_str+=str(api_response["assignedNumber"])
-display_text_on_screen(text_str, 94, -15)
+assigned_number_text+=str(api_response["assignedNumber"])
+log_this("Displaying assigned number on screen: " + assigned_number_text)
+display_assigned_number_on_screen(assigned_number_text, local_version)
 
 
 # #########################################################
